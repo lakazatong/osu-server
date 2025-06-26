bd243405c687f87a8a64b6d5ea0d0930e82ce628
lakazatong <Bo_wood@outlook.fr>
2025-06-26 23:00:14 +0200

    use local osu-framework-server

--- UseLocalFramework.ps1
+++ UseLocalFramework.ps1
@@ -12,14 +12,14 @@ dotnet remove $GAME_CSPROJ reference ppy.osu.Framework;
 dotnet remove $ANDROID_PROPS reference ppy.osu.Framework.Android;
 dotnet remove $IOS_PROPS reference ppy.osu.Framework.iOS;
 
-dotnet sln $SLN add ../osu-framework/osu.Framework/osu.Framework.csproj `
-    ../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj `
-    ../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj `
-    ../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj;
+dotnet sln $SLN add ../osu-framework-server/osu.Framework/osu.Framework.csproj `
+    ../osu-framework-server/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj `
+    ../osu-framework-server/osu.Framework.Android/osu.Framework.Android.csproj `
+    ../osu-framework-server/osu.Framework.iOS/osu.Framework.iOS.csproj;
 
-dotnet add $GAME_CSPROJ reference ../osu-framework/osu.Framework/osu.Framework.csproj;
-dotnet add $ANDROID_PROPS reference ../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj;
-dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj;
+dotnet add $GAME_CSPROJ reference ../osu-framework-server/osu.Framework/osu.Framework.csproj;
+dotnet add $ANDROID_PROPS reference ../osu-framework-server/osu.Framework.Android/osu.Framework.Android.csproj;
+dotnet add $IOS_PROPS reference ../osu-framework-server/osu.Framework.iOS/osu.Framework.iOS.csproj;
 
 # workaround for dotnet add not inserting $(MSBuildThisFileDirectory) on props files
 (Get-Content "osu.Android.props") -replace "`"..\\osu-framework", "`"`$(MSBuildThisFileDirectory)..\osu-framework" | Set-Content "osu.Android.props"
@@ -31,7 +31,7 @@ dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework
     Foreach-Object {
         if ($_ -match "</Project>")
         {
-            "  <Import Project=`"`$(MSBuildThisFileDirectory)../osu-framework/osu.Framework.iOS.props`"/>"
+            "  <Import Project=`"`$(MSBuildThisFileDirectory)../osu-framework-server/osu.Framework.iOS.props`"/>"
         }
 
         $_
@@ -40,16 +40,16 @@ dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework
 $TMP=New-TemporaryFile
 
 $SLNF=Get-Content "osu.Desktop.slnf" | ConvertFrom-Json
-$SLNF.solution.projects += ("../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj")
+$SLNF.solution.projects += ("../osu-framework-server/osu.Framework/osu.Framework.csproj", "../osu-framework-server/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj")
 ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
 Move-Item -Path $TMP -Destination "osu.Desktop.slnf" -Force
 
 $SLNF=Get-Content "osu.Android.slnf" | ConvertFrom-Json
-$SLNF.solution.projects += ("../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj")
+$SLNF.solution.projects += ("../osu-framework-server/osu.Framework/osu.Framework.csproj", "../osu-framework-server/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework-server/osu.Framework.Android/osu.Framework.Android.csproj")
 ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
 Move-Item -Path $TMP -Destination "osu.Android.slnf" -Force
 
 $SLNF=Get-Content "osu.iOS.slnf" | ConvertFrom-Json
-$SLNF.solution.projects += ("../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj")
+$SLNF.solution.projects += ("../osu-framework-server/osu.Framework/osu.Framework.csproj", "../osu-framework-server/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework-server/osu.Framework.iOS/osu.Framework.iOS.csproj")
 ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
 Move-Item -Path $TMP -Destination "osu.iOS.slnf" -Force
